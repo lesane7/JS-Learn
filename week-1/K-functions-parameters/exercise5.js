75b48988a6a75bca9b236d279f60c6040636f0f3
CodeYourFuture <elkinalarca@gmail.com>
2019-07-19 01:44:07 -0500

    To ends week_1

--- week-1/K-functions-parameters/exercise5.js
+++ week-1/K-functions-parameters/exercise5.js
@@ -1,6 +1,7 @@
 // Declare your function here
 function createLongGreeting(greeting, age) {
-    return 'Hello, my name is '+greeting+ ' '+"and I'm "+ age+ ' years old';
+    var message = 'Hello, my name is '+greeting+ ' '+"and I'm "+ age+ ' years old';
+    return message;
 }
 
 const greeting = createLongGreeting("Daniel", 30);
