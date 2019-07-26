2bc9f019d9ad0081b237f1c80dea8e72da22a8b1
CodeYourFuture <elkinalarca@gmail.com>
2019-07-25 19:34:42 -0500

    Improve code1

--- week-3-practise/level-3/1-practise-all.js
+++ week-3-practise/level-3/1-practise-all.js
@@ -36,24 +36,30 @@ Passwords must
 - Have non-alphanumeric symbols ("!", "#", "$", "%", ".")
 
 Passwords must not be any previous password in previousPassword Array. 
-PreviousPassword = ["fhD8!yrjj", "ttkTu.wer3", "dvyyeyY!5", "qwbfj76%", "tytT3729."];
+PreviousPassword = ["fhD8!yrjj", "ttkTu.wer3", "dvyyeyY!5", "qwbfj76%", "tryT3729."];
 
 Expected Result:
 PasswordValidationResult=  [false, false, false, false, true]
 
 */
 
-var password = ["Se%5", "TktE.TJTU", "384HsHF", "dvyyeyY!5", "tryT3729."];
+var password = ["Se%5", "TktE.TJTU", "384HsHF", "dvyyeyY!5", "tytT3729."];
 
 let er1 = /[0-9]/;
 let er2 = /[a-z]/;
 let er3 = /[A-Z]/;
 let er4 = /[!, #, $, %, .]/;
 
+PreviousPassword = ["fhD8!yrjj", "ttkTu.wer3", "dvyyeyY!5", "qwbfj76%", "tryT3729."];
+
+
 PasswordValidationResult = password.map(item => er1.test(item) && er2.test(item) && er3.test(item) && item.length > 5); //valida si en el rango de password contiene los caracteres y retorna booleano
 
 PasswordValidationResult2 = password.map(item => er1.test(item) && er2.test(item) && er3.test(item) && item.length > 5 && er4.test(item)); 
 
+PasswordValidationResult3 = password.map(item => er1.test(item) && er2.test(item) && er3.test(item) && item.length > 5 && er4.test(item) && !PreviousPassword.includes(item)); 
+ 
 
 console.log(PasswordValidationResult);
 console.log(PasswordValidationResult2);
+console.log(PasswordValidationResult3);
